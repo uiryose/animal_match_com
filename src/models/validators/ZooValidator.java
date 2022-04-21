f634a9c6b81db5a1739e2b210fb51491c40839bb
uiryose <ytpaisen@gmail.com>
2022-04-22 00:14:17 +0900

    ZooValidator、ZooService作成

---
+++ src/models/validators/ZooValidator.java
@@ -0,0 +1,90 @@
+package models.validators;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import actions.views.ZooView;
+import constants.MessageConst;
+
+
+/**
+ * 動物園インスタンスニ設定されている値のバリデーションを行うクラス
+ *
+ */
+public class ZooValidator {
+
+
+    /**
+     * 動物園インスタンスの各項目についてバリデーションを行う
+     * @param zv 画面に入力された動物園インスタンス
+     * @return エラーのリスト
+     */
+    public static List<String> validate(ZooView zv){
+        List<String> errors = new ArrayList<String>();
+
+        //動物園名のチェック
+        String nameError = validateName(zv.getZooName());
+        if(!nameError.equals("")) {
+            errors.add(nameError);
+        }
+
+        //所在地のチェック
+        String regionError = validateRegion(zv.getRegion());
+        if(!regionError.equals("")) {
+            errors.add(regionError);
+        }
+
+        //電話番号のチェック
+        String phoneError = validatePhone(zv.getPhone());
+        if(!phoneError.equals("")) {
+            errors.add(phoneError);
+        }
+
+        return errors;
+    }
+
+
+    /**
+     * 名前に入力値があるかチェックし、入力値がなければエラーメッセージを返却
+     * @param zooName
+     * @return エラーメッセージ
+     */
+    private static String validateName(String zooName) {
+        if(zooName == null || zooName.equals("")) {
+            return  MessageConst.E_NOZOONAME.getMessage();
+        }
+
+        //入力値がある場合は空文字を返却
+        return "";
+    }
+
+    /**
+     * 所在地に３～４文字の都道府県が入力されているかチェックし、入力値がなければエラーメッセージを返却
+     * @param region 都道府県名
+     * @return エラーメッセージ
+     */
+    private static String validateRegion(String region) {
+        if(!region.matches("^(東京都|北海道|(京都|大阪)府|.{2,3}県)$")) {
+            return MessageConst.E_NOREGION.getMessage();
+        }
+        //正しい入力値がある場合は空文字を返却
+        return "";
+
+    }
+
+
+    /**
+     * 電話番号(数字のみチェック)が入力されているかチェックし、エラーを返す
+     * @param phone
+     * @return
+     */
+    private static String validatePhone(String phone) {
+        if(phone.matches("^[0-9]+$")) {
+            return MessageConst.E_NOPHONE.getMessage();
+        }
+        return "";
+
+    }
+
+
+}
---
+++ src/services/ZooService.java
@@ -0,0 +1,109 @@
+package services;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import actions.views.ZooConverter;
+import actions.views.ZooView;
+import models.Zoo;
+import models.validators.ZooValidator;
+
+/**
+ * 動物園テーブルの操作に関わる処理を行うクラス
+ */
+public class ZooService extends ServiceBase {
+
+
+
+    /**
+     * idを条件に取得したデータをZooのインスタンスで返却する
+     * @param id
+     * @return 取得データのインスタンス
+     */
+    public ZooView findOne(int id) {
+        Zoo z = findOneInternal(id);
+        return ZooConverter.toView(z);
+    }
+
+
+    /**
+     * 画面から入力された動物園の登録内容を元にデータを1件作成し、動物園テーブルに登録する
+     * @param zv
+     * @return
+     */
+    public List<String> create(ZooView zv){
+        List<String> errors = ZooValidator.validate(zv);
+        if(errors.size()==0) {
+            LocalDateTime ldt = LocalDateTime.now();
+            zv.setCreatedAt(ldt);
+            zv.setUpdatedAt(ldt);
+            createInternal(zv);
+        }
+
+        //バリデーションで発生したエラーを返却（エラーがなければ0件の空リスト）
+        return errors;
+    }
+
+
+    /**
+     * 画面から入力された動物園の登録内容を元に、動物園データを更新する
+     * @param zv
+     * @return
+     */
+    public List<String> update(ZooView zv){
+
+        //バリデーションを行う
+        List<String> errors = ZooValidator.validate(zv);
+
+        if(errors.size() == 0) {
+
+            //更新日時を現在時刻に設定
+            LocalDateTime ldt = LocalDateTime.now();
+            zv.setUpdatedAt(ldt);
+
+            updateInternal(zv);
+        }
+        //バリデーションで発生したエラーを返却（エラーがなければ0件の空リスト）
+        return errors;
+
+    }
+
+
+
+    /**
+     * idを条件にデータを1件取得し、Zooのインスタンスで返却する
+     * @param id
+     * @return 取得データのインスタンス
+     */
+    private Zoo findOneInternal(int id) {
+        Zoo z = em.find(Zoo.class, id);
+        return z;
+    }
+
+
+    /**
+     * 動物園データを1件登録する
+     * @param zv 動物園データ
+     */
+    private void createInternal(ZooView zv) {
+
+        em.getTransaction().begin();
+        em.persist(ZooConverter.toModel(zv));
+        em.getTransaction().commit();
+    }
+
+
+    /**
+     * 動物園データを更新する
+     * @param zv 画面から入力された動物園データ
+     */
+    private void updateInternal(ZooView zv) {
+        em.getTransaction().begin();
+        Zoo z = findOneInternal(zv.getId());
+        ZooConverter.copyViewToModel(z, zv);
+        em.getTransaction().commit();
+    }
+
+
+
+}
